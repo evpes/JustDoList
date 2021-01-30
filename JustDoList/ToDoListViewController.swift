b1341ff378f89c0c674fb1be614acfa93a9fe665
Evgeny Pesterev <apsterio.anima@gmail.com>
2021-01-30 23:16:45 +0500

    Add "addButton" and logic

--- JustDoList/ToDoListViewController.swift
+++ JustDoList/ToDoListViewController.swift
@@ -8,8 +8,8 @@
 import UIKit
 
 class ToDoListViewController: UITableViewController {
-
-    let itemAray = ["Buy apples","Buy sourcream","Go to pool"]
+    
+    var itemAray = ["Buy apples","Buy sourcream","Go to pool"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -29,8 +29,36 @@ class ToDoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(itemAray[indexPath.row])
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType  = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType  = .checkmark
+        }
     }
+    
+    //MARK -  add new items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
 
-
+        let alert = UIAlertController(title: "Add new ToDo task", message: "Enter item name", preferredStyle: .alert)
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Task name"
+        }
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in
+            if let textField = alert.textFields![0].text {
+                if textField.count > 0 {
+                self.itemAray.append("\(textField)")
+                self.tableView.reloadData()
+                }
+            }
+        }))
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
+    
+    
 }
 
